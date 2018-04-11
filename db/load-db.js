41f85d8e84ec24e73979a34aef7158c0e8bf5aa5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-11 16:44:52 -0700

    wrote a script that adds a podcast name to the podcasts table

---
+++ db/load-db.js
@@ -0,0 +1,29 @@
+const client = require('../lib/db-client');
+
+const podcasts = ['The Read', 'She Explores', 'Welcome to Night Vale'];
+
+const podPromises = podcasts.map(podcast => {
+    return client.query(
+        `INSERT INTO podcasts(name)
+        VALUES($1)
+        ON CONFLICT DO NOTHING:`,
+        [podcast]
+    );
+});
+
+Promise.all(podPromises)
+    .then(() => {
+        return client.query(`
+            SELECT * FROM podcasts;
+        `);
+    })
+    .then(result => {
+        console.log(result.row);
+    })
+    .then(
+        () => console.log('DATABASE LOADED!'),
+        err => console.error(err)
+    )
+    .then(() => {
+        client.end();
+    });
