974d1229eb8ee49e42189a2531ab790a3c3303e2
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-11 16:37:23 -0700

    start test

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: '.test/.env.test' });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const client = require('../lib/db-client');
+const app = require('../lib/app');
+
+const { assert } = chai;
+chai.use(chaitHttp);
+
