51a0cbb8ada9e5a5ed976ca94969f77d8e4490c4
Steele Walston <steele.walston@gmail.com>
2018-04-11 16:42:21 -0700

    create and drop tables finished

---
+++ db/drop-tables.js
@@ -0,0 +1,7 @@
+const client = require('../lib/db-client');
+
+client.query(`
+    DROP TABLE pokemons;
+`)
+    .then(() => console.log('drop successful')), err => console.error(err)
+    .then(() => client.end());
