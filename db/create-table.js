095d3f35cdfed87f19614ad46d333a1d233ac1e2
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-11 17:10:34 -0700

    create database client

--- db/create-table.js
+++ db/create-table.js
@@ -3,16 +3,16 @@ const client = require('../lib/db-client');
 client.query(`
     CREATE TABLE IF NOT EXISTS birds(
         id SERIAL PRIMARY KEY,
-        common_name VARCHAR(256)
-        scientific_name VARCHAR(256)
-        wingspan VARCHAR(256)
-        diet VARCHAR(256)
+        common_name VARCHAR(256),
+        scientific_name VARCHAR(256),
+        wingspan VARCHAR(256),
+        diet VARCHAR(256),
         colors VARCHAR(256)[]
     );
 `)
     .then(
         /* eslint-disable no-console */
-        () => console.log('table created'),
+        () => console.log('table created!'),
         err => console.error(err)
         /* eslint-enable no-console */
     )
