f5dbb0dbc9101fbd08d5ff00565a31051b85646d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-11 17:03:30 -0700

    Updated podcasts to populate all columns

--- db/load-db.js
+++ db/load-db.js
@@ -1,13 +1,13 @@
 const client = require('../lib/db-client');
 
-const podcasts = ['The Read', 'She Explores', 'Welcome to Night Vale'];
+const podcasts = [{ name:'The Read', host: 'Kid Fury & Crissle', category:'Comedy' }, { name: 'She Explores', host: 'Gale Straub', category: 'Outdoor' }, { name: 'Welcome to Night Vale', host: 'Cecil Plamer', category: 'Comedy' }];
 
 const podPromises = podcasts.map(podcast => {
     return client.query(
-        `INSERT INTO podcasts(name)
-        VALUES($1)
+        `INSERT INTO podcasts(name, host, category)
+        VALUES($1, $2, $3)
         ON CONFLICT DO NOTHING;`,
-        [podcast]
+        [podcast.name, podcast.host, podcast.category]
     );
 });
 
