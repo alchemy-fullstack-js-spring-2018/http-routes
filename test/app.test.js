4cc20d42f9abd63d998918c1eb53690558e1869b
Charlie <heinercj@gmail.com>
2018-04-11 23:23:16 -0700

    can add book to db

---
+++ test/app.test.js
@@ -0,0 +1,45 @@
+require('dotenv').config({ path: './test/.env.test' });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const client = require('../lib/db-client');
+const app = require('../lib/app');
+
+const { assert } = chai;
+chai.use(chaiHttp);
+
+describe('books', () => {
+
+    before(() => client.query('DELETE FROM books'));
+
+    after(() => {
+        client.end();
+    });
+
+    let misery = {
+        title: 'Misery',
+        author: 'Stephen King'
+    };
+
+    let deadZone = {
+        title: 'The Dead Zone',
+        author: 'Stephen King'
+    };
+
+    before(() => {
+        return chai.request(app)
+            .post('/books')
+            .send(misery)
+            .then(({ body }) => {
+                assert.equal(body.title, misery.title);
+                assert.equal(body.author, misery.author);
+                misery = body;
+            });       
+    });
+
+    it('saves a pet', () => {
+        assert.ok(misery.id);
+    });
+
+
+
+});
