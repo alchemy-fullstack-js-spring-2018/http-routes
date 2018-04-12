9781d8bb19ee9ce22be2ae367fb87aa49cf5ff92
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-12 09:14:15 -0700

    create db files, first test passes

--- test/app.test.js
+++ test/app.test.js
@@ -5,8 +5,32 @@ const client = require('../lib/db-client');
 const app = require('../lib/app');
 
 const { assert } = chai;
-chai.use(chaitHttp);
+chai.use(chaiHttp);
 
 describe('blazers', () => {
-    
-})
+    before(() => client.query('DELETE FROM blazers'));
+
+    let lillard = {
+        name: 'Damian Lillard',
+        category_id: 1,
+        school: 'Weber State',
+        position: 'Point Guard'
+    };
+
+    before(() => {
+        return chai.request(app)
+            .post('/blazers')
+            .send(lillard)
+            .then(({ body }) => {
+                assert.equal(body.name, lillard.name);
+                assert.equal(body.category_id, lillard.category_id);
+                assert.equal(body.school, lillard.school);
+                assert.equal(body.position, lillard.position);
+                lillard = body;
+            });
+    });
+
+    it('saves a player', () => {
+        assert.ok(lillard.id);
+    });
+});
