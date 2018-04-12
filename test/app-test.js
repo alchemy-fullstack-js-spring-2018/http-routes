183a77687b67be015b918657f5f3c86e0fb537b6
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-12 11:16:06 -0700

    first test fail

--- test/app-test.js
+++ test/app-test.js
@@ -7,3 +7,27 @@ const client = require('../lib/db-client');
 const { assert } = chai;
 chai.use(chaiHttp);
 
+describe('E2E', () => {
+
+    before(() => client.query('DELETE FROM bands'));
+
+    let testBand = {
+        name: 'Bat For Lashes',
+        genre: 'Dreampop',
+        singer: 'Natasha Khan'
+    };
+
+    it('test', () => {
+        return chai.request(app)
+            .post('/bands')
+            .send(testBand)
+            .then(({ body }) => {
+                assert.equal(body.name, testBand.genre);
+            });       
+    });
+
+    after(() => {
+        client.end();
+    });
+
+});
