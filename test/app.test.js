67f968f66d259c03f6f68d6da0da51ee46b3fa43
Ryan Manro <rmanro@gmail.com>
2018-04-11 19:14:12 -0700

    GET all testing OK

---
+++ test/app.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const client = require('../lib/db-client');
+const app = require('../lib/app');
+
+const { assert } = chai;
+chai.use(chaiHttp);
+
+describe('Bands', () => {
+
+    it('Gets all bands', () => {
+        return chai.request(app)
+            .get('/bands')
+            .then(({ body }) => {
+                assert.deepEqual(body, [{ id: 1, name: 'Preoccupations' },
+                    { id: 2, name: 'Shame' },
+                    { id: 3, name: 'Ought' }]);
+            });
+    });
+
+    after(() => {
+        client.end();
+    });
+
+});
