caa758ea9253806d529d4e365d5f11d4a00760eb
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-16 22:35:54 -0700

    building app-test.js

---
+++ test/app-test.js
@@ -0,0 +1,8 @@
+require('dotenv').config({ path: './test/.env.test'});
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const client = require('../lib/db-client');
+const app = require('../lib/app');
+
+const { assert } = chai;
+chai.use(chaiHttp);
