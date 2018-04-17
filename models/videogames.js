0550573a99228c4d90c65d62c0db1cfa0dd43e4a
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-16 23:38:20 -0700

    added queries to videogame.js data model

--- models/videogames.js
+++ models/videogames.js
@@ -1,1 +1,48 @@
 const client = require('../lib/db-client');
+
+module.exports = {
+    selectAll() {
+        return client.query('SELECT * FROM videogames')
+            .then(({ rows }) => rows);
+    },
+    selectOne(id) {
+        return client.query(`
+            SELECT * 
+            FROM videogames
+            WHERE id = $1;
+        `,
+        [id]
+        ).then(({ rows }) => rows[0]);
+    },
+    insert(videogames) {
+        return client.query(`
+            INSERT INTO videogames (
+                title, developer, category_id
+            )
+            VALUES ($1, $2, $3)
+            RETURNING *;
+        `,
+        [videogames.title, videogames.developer, videogames.category_id]
+        ).then(({ rows }) => rows[0]);
+    },
+    update(videogames) {
+        return client.query(`
+            UPDATE videogames 
+            SET    
+                title = $1, 
+                developer = $2, 
+                category_id = $3, 
+            RETURNING *;
+        `,
+        [videogames.name, videogames.color, videogames.category_id, videogames.description]
+        ).then(({ rows }) => rows[0]);
+    },
+    delete(id) {
+        return client.query(`
+            DELETE FROM videogames
+            WHERE id = $1;
+        `,
+        [id]
+        ).then(() => null);
+    }
+};
