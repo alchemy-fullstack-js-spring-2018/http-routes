d3dfb276058702f564d7988e3ef727aa60452ceb
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-16 23:29:40 -0700

    finished core methods

--- test/app-test.js
+++ test/app-test.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './test/.env.test'});
+require('dotenv').config({ path: './test/.env.test' });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const client = require('../lib/db-client');
